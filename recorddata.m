2042147c2772631c1a52c882aec2f6eb6c02ec43
Vrushhabh <vpate81@illinois.edu>
2021-06-18 15:27:07 -0500

    Change the name of recordtime to a better suited name recordtddata. Add parameters  and comment of the description of the function.

---
+++ recorddata.m
@@ -0,0 +1,11 @@
+function [accuracy, results] = recorddata(trial_size,complexity)
+%REACTIONEXPERIMENT This function makes figures that either contain the
+%target or don't and the user has to quickly determine that by using the
+%'A' (contains target) and 'S' (does not contain target). 
+% This function returns a vector of reaction times and a float that
+% determines the accuracy 
+accuracy = 0;
+results = [];
+
+
+
