c0ed11b37d1e068c3affaf18937cfabfb024292d
Vrushhabh <vpate81@illinois.edu>
2021-06-19 22:23:47 -0500

    Make DataDisplayer class that will take data from the recorddata function to make graphs of comparison between individuals

---
+++ DataDisplayer.m
@@ -0,0 +1,17 @@
+classdef DataDisplayer
+    % Displays data from the recorddata function
+    
+    properties
+        %Each index corresponds to one trial from one person
+        %This makes code in the functions of this class really 
+        %intuitive and easy to follow
+        accuracies = []
+        correctResults = [] 
+        wrongResults = []
+        names = []
+    end
+    
+    methods
+    end
+end
+
