bb69b82c352a2633e5ea4db0182e0d5e434d01bc
Vrushhabh <vpate81@illinois.edu>
2021-06-19 22:04:06 -0500

    Finish reccorddata function by adding more return variables (for data visualization) and adding comments to remove any possible confusion in the future

--- recorddata.m
+++ recorddata.m
@@ -1,4 +1,4 @@
-function [correctResults] = recorddata(trial_size,complexity)
+function [accuracy, correctResults, wrongResults] = recorddata(trial_size, complexity)
 %REACTIONEXPERIMENT This function makes figures that either contain the
 %target or don't and the user has to quickly determine that by using the
 %'A' (contains target) and 'S' (does not contain target) keys. 
@@ -7,33 +7,32 @@ function [correctResults] = recorddata(trial_size,complexity)
 % as ubject to be tested and complexity determines how many objects the
 % subject will have to process (higher the complexity the higher the
 % reaction time
-accuracy = 0;
 correctResults = [];
 wrongResults = [];
 correct = 0;
 i=0;
-while i < trial_size
-    random = rand();
-    if random < .5
+while i < trial_size %each iteration through this loop makes a data point
+    random = rand(); %this function is used to make a 50/50 between figures
+    if random < .5   %that contain the figure and don't
         plot(rand(),rand(),'o')
-        hold on
+        hold on     
         x = rand(1,complexity);
         y = rand(1,complexity);
         plot(x,y, 'x')
-        tic
-        key = waitforbuttonpress;
+        tic         
+        key = waitforbuttonpress; %pauses the program to wait for user input
         input = double(get(gcf,'CurrentCharacter'));
-        if  input == 97  || input == 65
-             time = toc;
+        if  input == 97  || input == 65 % 65 and 97 represent the A key in
+             time = toc;                %ASCII 
              correct = correct + 1;
-             correctResults(end+1) = time;
+             correctResults(end+1) = time; %adds data to data matrix 
         else 
             time = toc;
             wrongResults(end+1) = time;
             
         end
         
-    else 
+    else                  %else represents the figures without the target 
         x = rand(1,complexity);
         y = rand(1,complexity);
         plot(x,y, 'x')
@@ -51,7 +50,9 @@ while i < trial_size
     end
     clf;
     i = i + 1;
+    
 end
+accuracy = (correct/ trial_size)
 
 
 
